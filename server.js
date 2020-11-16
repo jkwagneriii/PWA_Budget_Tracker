5ac2287c9e5095699735cea305f52891a9393e7a
Jake Wagner <jkwagneriii@gmail.com>
2020-11-16 16:52:45 -0600

    bugz

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI ||"mongodb+srv://jakeWagner:<password>@cluster0.7f5sx.mongodb.net/<dbname>?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGODB_URI ||"mongodb+srv://jakeWagner:12345@cluster0.7f5sx.mongodb.net/<dbname>?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
