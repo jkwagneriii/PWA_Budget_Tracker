1c8535b7d06632607cd93db58039b21ae2772574
Jake Wagner <jkwagneriii@gmail.com>
2020-11-16 16:44:51 -0600

    atlas

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI ||"mongodb+srv://jakeWagner:<password>@cluster0.7f5sx.mongodb.net/<dbname>?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
